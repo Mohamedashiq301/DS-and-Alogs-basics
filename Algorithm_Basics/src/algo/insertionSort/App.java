3102d2e2392c1c2792f3b94f451ce94fbd77b3c8
Mohamedashiq301 <mohamedashiq301@gmail.com>
2022-02-08 22:14:17 +0530

    Added some files

--- Algorithm_Basics/src/algo/insertionSort/App.java
+++ Algorithm_Basics/src/algo/insertionSort/App.java
@@ -3,12 +3,22 @@ package algo.insertionSort;
 public class App {
 
 	public static void main(String[] args) {
-		int []myArray=insertionSorted(new int [] {2, 8, 3, 10, 4, 12, 87, 45, 99})
-	}
-	
-	public static int [] insertionSorted(int []a) {
-		for(int i=0; i<a.length;i++) {
-			
+		int[] myArray = insertionSorted(new int[] { 9,8,99,110,8,87,637,8,3,13,87,12,99 });
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.println(myArray[i]);
 		}
+	}
+
+	public static int[] insertionSorted(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			int element = a[i];
+			int j = i - 1;
+			while (j >= 0 && a[j] > element) {
+				a[j + 1] = a[j];
+				j--;
+			}
+			a[j+1]=element;
+		}
+		return a;
 	}
 }
